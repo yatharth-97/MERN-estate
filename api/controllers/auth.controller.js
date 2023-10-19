dc5328003c2979c8faf7250f6c713c8b65023bec
yatharth-97 <yatharthshahrawat97@gmail.com>
2023-10-19 10:32:55 +0530

    created signup api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,3 +1,15 @@
-export const signup = (req, res) => {
-  console.log(req.body);
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+
+export const signup = async (req, res) => {
+  // we want to save username,email, password in database
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save(); // created new db
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 };
