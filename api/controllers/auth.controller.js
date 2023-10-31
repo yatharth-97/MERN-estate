23786da1acca15d9a8e2f90be29c84a27b86dac4
yatharth-97 <yatharthshahrawat97@gmail.com>
2023-10-31 21:28:25 +0530

    [FEAT] added middleware for handling errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,18 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req, res) => {
-  // we want to save username,email, password in database
+export const signup = async (req, res, next) => {
+  // we want to save username, email, password in database
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
+
   try {
     await newUser.save(); // created new db
     res.status(201).json({ message: 'User created successfully' });
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
+    // res.status(500).json(error.message);
   }
 };
--- api/index.js
+++ api/index.js
@@ -22,6 +22,17 @@ app.use(express.json());
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 
+// middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message, // message: message (after ES6 no need to do this)
+  });
+});
+
 // server running
 app.listen(3000, () => {
   console.log(`Server is running on port 3000`);
