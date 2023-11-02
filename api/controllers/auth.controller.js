f69601876e0efcffb8904cd99233e6f1218332bb
yatharth-97 <yatharthshahrawat97@gmail.com>
2023-11-02 11:59:55 +0530

    [WIP] Created sign in API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   // we want to save username, email, password in database
@@ -14,5 +15,24 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error);
     // res.status(500).json(error.message);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email }); // validUser is full db referring to email
+    if (!validUser) return next(errorHandler(404, 'User not found'));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc; // here we are extracting the password property and assigning its value to a new variable "pass". The ...rest syntax is used to capture all the remaining properties of the validUser object in the rest variable
+    res
+      .cookie('access_token', token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
